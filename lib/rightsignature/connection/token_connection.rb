d8c2aec0c520bb31a9425796889b4607d338884c
Si Le <omnisyle@gmail.com>
2021-04-14 13:46:41 -0400

    Changes base url and token auth

--- lib/rightsignature/connection/token_connection.rb
+++ lib/rightsignature/connection/token_connection.rb
@@ -1,36 +1,36 @@
 module RightSignature
   class TokenConnection
     include HTTParty
-    base_uri 'https://rightsignature.com'
-    format :xml
-    
+    base_uri 'https://api.rightsignature.com/public/v1'
+    format :json
+
     attr_reader :api_token
 
     # Creates new instance of RightSignature::TokenConnection to make API calls
-    # * <b>api_token</b>: API Token. 
-    #     
+    # * <b>api_token</b>: API Token.
+    #
     # Example:
     #   @rs_token = RightSignature::TokenConnection.new("APITOKEN")
-    # 
+    #
     def initialize(api_token)
       @api_token = api_token
     end
-    
+
     # Generates HTTP request with token credentials. Require api_token to be set.
     # * <b>method</b>: HTTP Method. Ex. ('get'/'post'/'delete'/'put')
     # * <b>url</b>: request path/url of request
     # * <b>options</b>: HTTPary options to pass. Last option should be headers
-    # 
+    #
     def request(method, url, options)
       raise "Please set api_token" if @api_token.nil? || @api_token.empty?
-      
+
       options[:headers] ||= {}
-      options[:headers]['api-token'] = @api_token
       options[:headers]["Accept"] ||= "*/*"
-      options[:headers]["content-type"] ||= "application/xml"
+      options[:headers]["Content-Type"] ||= "application/json"
+      options[:headers]["Authorization"] = "Basic #{Base64.strict_encode64(@api_token)}"
       self.class.__send__(method, url, options)
     end
-    
+
   end
 
 end
